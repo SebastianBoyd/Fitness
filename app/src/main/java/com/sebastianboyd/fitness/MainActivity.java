f407966173f61b825936164c6a6df870f1e51f01
SebastianBoyd <sebastian.boyd@drakeslc.org>
2015-01-14 14:39:52 -0800

    Read history

--- app/src/main/java/com/sebastianboyd/fitness/MainActivity.java
+++ app/src/main/java/com/sebastianboyd/fitness/MainActivity.java
@@ -15,6 +15,12 @@ import com.google.android.gms.common.Scopes;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.common.api.Scope;
 import com.google.android.gms.fitness.Fitness;
+import com.google.android.gms.fitness.data.DataType;
+import com.google.android.gms.fitness.request.DataReadRequest;
+
+import java.util.Date;
+import java.util.Calendar;
+import java.util.concurrent.TimeUnit;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -29,11 +35,19 @@ public class MainActivity extends ActionBarActivity {
     private static final String TAG = "Fit Auth";
     private boolean authInProgress = false;
     private GoogleApiClient mClient = null;
+    Calendar cal = Calendar.getInstance();
+    Date now = new Date();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        cal.setTime(now);
+        long endTime = cal.getTimeInMillis();
+        cal.add(Calendar.WEEK_OF_YEAR, -1);
+        long startTime = cal.getTimeInMillis();
+
         if (savedInstanceState != null) {
             authInProgress = savedInstanceState.getBoolean(AUTH_PENDING);
         }
@@ -108,6 +122,21 @@ public class MainActivity extends ActionBarActivity {
                 )
                 .build();
     }
+    // Setting a start and end date using a range of 1 week before this moment.
+    DataReadRequest readRequest = new DataReadRequest.Builder()
+            // The data request can specify multiple data types to return, effectively
+            // combining multiple data queries into one call.
+            // In this example, it's very unlikely that the request is for several hundred
+            // datapoints each consisting of a few steps and a timestamp.  The more likely
+            // scenario is wanting to see how many steps were walked per day, for 7 days.
+            .aggregate(DataType.TYPE_STEP_COUNT_DELTA, DataType.AGGREGATE_STEP_COUNT_DELTA)
+                    // Analogous to a "Group By" in SQL, defines how data should be aggregated.
+                    // bucketByTime allows for a time span, whereas bucketBySession would allow
+                    // bucketing by "sessions", which would need to be defined in code.
+            .bucketByTime(1, TimeUnit.DAYS)
+            // Replace 0 and 1 with startTime and endTime
+            .setTimeRange(0, 1, TimeUnit.MILLISECONDS)
+            .build();
 
     @Override
     protected void onStart() {
