690137bf4201f1eb8d90160e2314dfeac35cc227
SebastianBoyd <sebastian.boyd@drakeslc.org>
2015-02-06 14:23:02 -0800

    Gets fitness data and prints to console.

--- app/src/main/java/com/sebastianboyd/fitness/MainActivity.java
+++ app/src/main/java/com/sebastianboyd/fitness/MainActivity.java
@@ -3,6 +3,7 @@ package com.sebastianboyd.fitness;
 
 import android.content.Intent;
 import android.content.IntentSender;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.util.Log;
@@ -17,6 +18,7 @@ import com.google.android.gms.common.Scopes;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.common.api.Scope;
 import com.google.android.gms.fitness.Fitness;
+import com.google.android.gms.fitness.data.Bucket;
 import com.google.android.gms.fitness.data.DataPoint;
 import com.google.android.gms.fitness.data.DataSet;
 import com.google.android.gms.fitness.data.DataType;
@@ -27,6 +29,7 @@ import com.google.android.gms.fitness.result.DataReadResult;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 
@@ -59,6 +62,8 @@ public class MainActivity extends ActionBarActivity {
         }
 
         buildFitnessClient();
+        getData();
+        calculateLife();
     }
 
     /**
@@ -90,6 +95,7 @@ public class MainActivity extends ActionBarActivity {
                                 Log.i(TAG, "Connected!!!");
                                 // Now you can make calls to the Fitness APIs.
                                 // Put application specific code here.
+                                new InsertAndVerifyDataTask().execute();
                             }
 
                             @Override
@@ -147,6 +153,18 @@ public class MainActivity extends ActionBarActivity {
                 .build();
     }
 
+    private class InsertAndVerifyDataTask extends AsyncTask<Void, Void, Void> {
+        protected Void doInBackground(Void... params) {
+            DataReadRequest readRequest = getData();
+            DataReadResult dataReadResult =
+                    Fitness.HistoryApi.readData(mClient,
+                                                readRequest).await(1, TimeUnit.MINUTES);
+            printData(dataReadResult);
+            return null;
+        }
+
+    }
+
     @Override
     protected void onStop() {
         super.onStop();
@@ -155,13 +173,13 @@ public class MainActivity extends ActionBarActivity {
         }
     }
 
-    protected DataReadResult getData() {
+    protected DataReadRequest getData() {
         // Setting a start and end date using a range of 1 week before this moment.
         Calendar cal = Calendar.getInstance();
         Date now = new Date();
         cal.setTime(now);
         long endTime = cal.getTimeInMillis();
-        cal.add(Calendar.DAY_OF_YEAR, 0);
+        cal.add(Calendar.WEEK_OF_YEAR, -1);
         long startTime = cal.getTimeInMillis();
 
         SimpleDateFormat dateFormat = new SimpleDateFormat(DATE_FORMAT);
@@ -174,16 +192,14 @@ public class MainActivity extends ActionBarActivity {
                 // In this example, it's very unlikely that the request is for several hundred
                 // datapoints each consisting of a few steps and a timestamp.  The more likely
                 // scenario is wanting to see how many steps were walked per day, for 7 days.
-                .aggregate(DataType.TYPE_CALORIES_EXPENDED,
-                           DataType.AGGREGATE_STEP_COUNT_DELTA)
+                .aggregate(DataType.TYPE_STEP_COUNT_DELTA, DataType.AGGREGATE_STEP_COUNT_DELTA)
                         // Analogous to a "Group By" in SQL, defines how data should be aggregated.
                         // bucketByTime allows for a time span, whereas bucketBySession would allow
                         // bucketing by "sessions", which would need to be defined in code.
                 .bucketByTime(1, TimeUnit.DAYS)
                 .setTimeRange(startTime, endTime, TimeUnit.MILLISECONDS)
                 .build();
-        return Fitness.HistoryApi.readData(mClient, readRequest)
-                                 .await(1, TimeUnit.MINUTES);
+        return readRequest;
     }
 
     protected void addData() {
@@ -191,7 +207,7 @@ public class MainActivity extends ActionBarActivity {
     }
 
     protected void calculateLife() {
-        setContentView(R.layout.activity_pushup_counter);
+        setContentView(R.layout.activity_main);
         TextView life;
         life = (TextView) findViewById(R.id.life);
         life.setText("Hello");
@@ -200,6 +216,29 @@ public class MainActivity extends ActionBarActivity {
 
     protected void calculateMoney() {
 
+    }
+
+    private void printData(DataReadResult dataReadResult) {
+        // [START parse_read_data_result]
+        // If the DataReadRequest object specified aggregated data, dataReadResult will be returned
+        // as buckets containing DataSets, instead of just DataSets.
+        if (dataReadResult.getBuckets().size() > 0) {
+            Log.i(TAG, "Number of returned buckets of DataSets is: "
+                       + dataReadResult.getBuckets().size());
+            for (Bucket bucket : dataReadResult.getBuckets()) {
+                List<DataSet> dataSets = bucket.getDataSets();
+                for (DataSet dataSet : dataSets) {
+                    dumpDataSet(dataSet);
+                }
+            }
+        } else if (dataReadResult.getDataSets().size() > 0) {
+            Log.i(TAG, "Number of returned DataSets is: "
+                       + dataReadResult.getDataSets().size());
+            for (DataSet dataSet : dataReadResult.getDataSets()) {
+                dumpDataSet(dataSet);
+            }
+        }
+        // [END parse_read_data_result]
     }
 
     private void dumpDataSet(DataSet dataSet) {
