418ddd93e2692e442c1dac6def71cdf13f2c34f0
sebastian <lord.of.all.sebastian@gmail.com>
2015-02-14 13:38:54 -0800

    Increase Accuracy

--- app/src/main/java/com/sebastianboyd/fitness/AddJumpsActivity.java
+++ app/src/main/java/com/sebastianboyd/fitness/AddJumpsActivity.java
@@ -20,13 +20,14 @@ public class AddJumpsActivity extends BaseActivity implements
     private long lastUpdate = 0;
     private float last_x, last_y, last_z;
     private static final int SHAKE_THRESHOLD = 600;
+    private double jumps = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_jumps);
         senSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
-        senAccelerometer = senSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        senAccelerometer = senSensorManager.getDefaultSensor(Sensor.TYPE_LINEAR_ACCELERATION);
         senSensorManager.registerListener(this, senAccelerometer ,
                                           SensorManager.SENSOR_DELAY_NORMAL);
     }
@@ -57,21 +58,22 @@ public class AddJumpsActivity extends BaseActivity implements
     @Override
     public void onSensorChanged(SensorEvent event) {
         Sensor mySensor = event.sensor;
-        if (mySensor.getType() == Sensor.TYPE_ACCELEROMETER) {
+        if (mySensor.getType() == Sensor.TYPE_LINEAR_ACCELERATION) {
             float x = event.values[0];
             float y = event.values[1];
             float z = event.values[2];
 
             long curTime = System.currentTimeMillis();
 
-            if ((curTime - lastUpdate) > 100) {
+            if ((curTime - lastUpdate) > 150) {
                 long diffTime = (curTime - lastUpdate);
                 lastUpdate = curTime;
 
                 float speed = Math.abs(x + y + z - last_x - last_y - last_z)/ diffTime * 10000;
 
                 if (speed > SHAKE_THRESHOLD) {
-                    Log.v("Sensor", "Shaken");
+                    jumps = jumps + 0.5;
+                    Log.v("Sensor", String.valueOf(jumps));
                 }
 
                 last_x = x;
