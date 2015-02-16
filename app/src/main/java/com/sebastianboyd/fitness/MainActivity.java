613c74a05355700e3b83b0d577a4d7b29f70b9d6
sebastian <lord.of.all.sebastian@gmail.com>
2015-02-15 16:28:03 -0800

    Add income to preferences

--- app/src/main/java/com/sebastianboyd/fitness/MainActivity.java
+++ app/src/main/java/com/sebastianboyd/fitness/MainActivity.java
@@ -267,7 +267,8 @@ public class MainActivity extends BaseActivity {
         // as you specify a parent activity in AndroidManifest.xml.
         switch (item.getItemId()) {
             case R.id.action_settings:
-                return true;
+                Intent intent = new Intent(this, Preferences.class);
+                startActivity(intent);
         }
 
         return super.onOptionsItemSelected(item);
